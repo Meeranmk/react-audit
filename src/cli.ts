519d33e4e066dc51aa3d56754bda52d7d297913a
Meeranmk <startechnology282@gmail.com>
2026-08-10 12:00:24 +0530

    chore: sync VERSION constant to 1.1.0

--- src/cli.ts
+++ src/cli.ts
@@ -25,7 +25,7 @@ import { generateFixPrompt, getRankedGroups, FixGroup } from './reporters/prompt
 import { getRule, getRulesByCategory, getRuleCount } from './rules';
 import { CATEGORY_LABELS, RuleCategory, AuditResult } from './types';
 
-const VERSION = '1.0.2';
+const VERSION = '1.1.0';
 
 const program = new Command();
 
